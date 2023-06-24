b59241bfeaa06efcfbf377a2f95b850177dca202
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-06-24 08:01:38 -0500

    .

---
+++ debugger-embedded/debugger-embedded/debugger-embedded.ino
@@ -0,0 +1,18 @@
+class Pins {
+public:
+
+  void takeControl() {
+    
+  }
+
+};
+
+Pins pins;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
